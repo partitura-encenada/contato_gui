4252ddfa5cefb516bb42fa4e374f84403888b64e
Arthur Ranquine <aranquine@gmail.com>
2026-03-12 21:34:11 -0300

    12-03

--- src/ble_scanner.py
+++
@@ -1,6 +0,0 @@
-from bleak import BleakScanner
-from constants import BLE_MIDI_SERVICE_UUID
-
-
-async def scan_devices():
-    return await BleakScanner.discover(timeout=3.0, service_uuids=[BLE_MIDI_SERVICE_UUID])
